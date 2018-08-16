4d5a00cd74074922caf37dcc7830d1eed3c7a7ec
mubarakcoded <mubarakcoded@gmail.com>
2018-08-16 09:27:32 -0700

    Feat: Added mapbox api key

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 let restaurants,
   neighborhoods,
   cuisines
@@ -78,7 +79,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibXViYXJha2NvZGVkIiwiYSI6ImNqa2gyandrbDBuN2kzb2w3ZnhrcW83ejEifQ.9JISC1ct6M53D6ZMCbYTMA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
